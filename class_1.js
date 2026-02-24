d9f5d80917e39115e5baff578d855eebc6bacf98
nitya0859 <vishnoinitya338@gmail.com>
2026-02-24 16:23:19 +0530

    add

---
+++ class_1.js
@@ -0,0 +1,28 @@
+let a=5;
+let b=10;
+let linebreak="<br/>";
+document.write("a+b=");
+result = a+b;
+document.write(result);
+document.write(linebreak);
+
+document.write("a-b=");
+result = a-b;
+document.write(result);
+document.write(linebreak);
+
+document.write("a*b=");
+result = a*b;
+document.write(result);
+document.write(linebreak);
+
+document.write("a/b=");
+result = a/b;
+document.write(result);
+document.write(linebreak);
+
+document.write("a%b=");
+result = a%b;
+document.write(result);
+document.write(linebreak);
+
