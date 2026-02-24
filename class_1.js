a06cce48395220792b785cdc05649ec6d0228650
nitya0859 <vishnoinitya338@gmail.com>
2026-02-24 16:15:14 +0530

    add

--- class_1.js
+++
@@ -1,6 +0,0 @@
-<script type="text/javascript">
-var num=10;
-console.log(num);
-console.log("Hello World");
-num=false;
-</script>
--- script.js
+++ script.js
@@ -1,14 +1,4 @@
 console.log("Hello World");
 document.write("Hello World");
 
-var num=10;
-console.log(num);
-console.log("Hello World");
-num=false;
 
-const sym1=Symbol(4);
-console.log(sym1);
-const sym2=Symbol(4);
-if(sym1===sym2){
-    console.log("true");
-}
