3dc5d6ad70422236d86dedb977cd9b77135d37cf
nitya0859 <vishnoinitya338@gmail.com>
2026-03-05 23:03:49 +0530

    Class work

---
+++ comp/script.js
@@ -0,0 +1,27 @@
+var a=10;
+var b=20;
+var linebreak="</br>";
+
+console.log("(a==b)=>");
+result =(a==b);     //returns false
+console.log(result);
+
+console.log("a!=b=>");
+result =(a!=b);
+console.log(result);
+
+console.log("a>b=>");
+result =(a>b);
+console.log(result);
+
+console.log("a<b=>");
+result=(a<b);
+console.log(result);
+
+console.log("a>=b=>");
+result=(a>=b);
+console.log(result);
+
+console.log("a<=b=>");
+result=(a<=b);
+console.log(result);
