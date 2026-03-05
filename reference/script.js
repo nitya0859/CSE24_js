14a907a4f4df5863d93c8a5a25b4edb592d2a4cc
nitya0859 <vishnoinitya338@gmail.com>
2026-03-05 23:00:59 +0530

    Class work

---
+++ reference/script.js
@@ -0,0 +1,20 @@
+//function
+ var sayHello = function() {
+    alert('Hello World'); }
+    console.log(sayHello);
+
+
+//array
+var numberArray = [1,2,3];
+var animals = new Array('dog', 'cat', 'tiger');
+console.log(numberArray);
+console.log(animals);
+
+//object
+var person = {
+    name: 'Nitya Vishnoi',
+    age: 19,
+    title: 'Student of ABESEC' 
+}
+
+console.log(person);
