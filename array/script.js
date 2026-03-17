63ce98abea3904a92c91b8adf15cc6cdea1ab811
nitya0859 <vishnoinitya338@gmail.com>
2026-03-17 15:30:25 +0530

    code executed

---
+++ array/script.js
@@ -0,0 +1,15 @@
+let collection={
+    {},
+    [],
+    true,
+    "john",
+    function(){},
+    9000893888,
+    undefined,
+    null,
+    newString("abc"),
+    new Date(),
+};
+console.log(collection);
+collection.teacherName="John Mac";
+collection.phoneNo=438009093090;
---
+++ object/script.js
@@ -0,0 +1,20 @@
+var person1={
+    name:John,
+    age:30,
+    isMarried:true,
+    address:{
+        street:'vinay nagar',
+        flatNO:201
+    }
+}
+
+var person2=newObject();
+person2.name="krishnkant";//to add properties to this object
+person2.age=32;
+person2.isMarried=true;
+person2.address={};
+person2.address.street="nagar road";
+person2['address']['flatNO']=33;
+console.log(
+    
+)
