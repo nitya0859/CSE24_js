60d200383ec281c5adcd952b55a82efff8518032
nitya0859 <vishnoinitya338@gmail.com>
2026-03-17 16:02:19 +0530

    code executed

--- dom/script.js
+++ dom/script.js
@@ -3,9 +3,9 @@ x.innerHTML="hii";
 //returns only one element
 //other two methods
 let y=document.getElementByClassName("demo1"); //this returns array 
-y.innerHTML="oky";
+y[0].innerHTML="oky";
 let z=document.getElementsByTagName("h2"); //this too in array form
-z.innerHTML="yyyy";
+z[0].innerHTML="yyyy";
 let a=document.querySelector(""); // .class, #id, tag(directly) anything can be given
 let b=document.querySelectorAll(""); // .class, #id, tag(directly) anything can be given
 //the query selector gives u the 1st value encountered 
---
+++ dom_2/script.js
@@ -0,0 +1,4 @@
+let x=document.getElementById("demo").getAttribute("id");
+//x=demo
+let y=document.getElementById("demo").setAttribute("class","democlass");
+//class=democlass
